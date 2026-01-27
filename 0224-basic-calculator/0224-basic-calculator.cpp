503678895ab4aa024e44f9be3c189bd5d5443cba
Chintha Sri Vaishnavi <24A95A0515@aec.edu.in>
2026-01-27 20:02:02 +0530

    Time: 0 ms (100%), Space: 13.9 MB (22.57%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.cpp
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -1,65 +1,44 @@
 class Solution {
 public:
     int calculate(string s) {
-        stack<long long> nums;
-        stack<char> ops;
-        int n = s.size();
+        long long result = 0;
+        long long num = 0;
+        int sign = 1;
+        stack<long long> st;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < s.size(); i++) {
             char ch = s[i];
 
-            if (ch == ' ')
-                continue;
             if (isdigit(ch)) {
-                long long num = 0;
-                while (i < n && isdigit(s[i])) {
-                    num = num * 10 + (s[i] - '0');
-                    i++;
-                }
-                i--;
-                nums.push(num);
-            } else if (ch == '(') {
-                ops.push(ch);
-            } else if (ch == '+' || ch == '-') {
-
-                int j = i - 1;
-                while (j >= 0 && s[j] == ' ')
-                    j--;
-
-                if (j < 0 || s[j] == '(' || s[j] == '+' || s[j] == '-') {
-                    nums.push(0);
-                }
-
-                while (!ops.empty() && ops.top() != '(') {
-                    apply(nums, ops);
-                }
-                ops.push(ch);
-            } else if (ch == ')') {
-                while (!ops.empty() && ops.top() != '(') {
-                    apply(nums, ops);
-                }
-                ops.pop();
+                num = num * 10 + (ch - '0');
             }
+            else if (ch == '+') {
+                result += sign * num;
+                num = 0;
+                sign = 1;
+            }
+            else if (ch == '-') {
+                result += sign * num;
+                num = 0;
+                sign = -1;
+            }
+            else if (ch == '(') {
+                st.push(result);
+                st.push(sign);
+                result = 0;
+                sign = 1;
+            }
+            else if (ch == ')') {
+                result += sign * num;
+                num = 0;
+
+                result *= st.top(); st.pop(); // sign
+                result += st.top(); st.pop(); // previous result
+            }
+            // spaces ignored
         }
-        while (!ops.empty()) {
-            apply(nums, ops);
-        }
 
-        return (int)nums.top();
-    }
-
-private:
-    void apply(stack<long long>& nums, stack<char>& ops) {
-        long long b = nums.top();
-        nums.pop();
-        long long a = nums.top();
-        nums.pop();
-        char op = ops.top();
-        ops.pop();
-
-        if (op == '+')
-            nums.push(a + b);
-        else
-            nums.push(a - b);
+        result += sign * num;
+        return (int)result;
     }
 };
