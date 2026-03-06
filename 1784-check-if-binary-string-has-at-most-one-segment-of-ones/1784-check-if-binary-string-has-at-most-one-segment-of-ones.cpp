7ad5d50b667831baf6e7818d1b770985a81272a1
Chintha Sri Vaishnavi <24A95A0515@aec.edu.in>
2026-03-06 13:46:08 +0530

    Time: 0 ms (100%), Space: 8.3 MB (46.93%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     bool checkOnesSegment(string s) {
-        int mcount=0;
-        // if(s[0]=='1') count++;
-        int i=0;
-        while(i<s.length()){
-            int count=0;
-            while(i<s.length() && s[i]=='1'){
-              count++;
-              i++;
-            }
-            if(count>0)mcount++;
-            i++;
-        }
-        return mcount==1;
+        // int mcount=0;
+        // // if(s[0]=='1') count++;
+        // int i=0;
+        // while(i<s.length()){
+        //     int count=0;
+        //     while(i<s.length() && s[i]=='1'){
+        //       count++;
+        //       i++;
+        //     }
+        //     if(count>0)mcount++;
+        //     i++;
+        // }
+        // return mcount==1;
+         return s.find("01") == string::npos;
        
     }
 };
