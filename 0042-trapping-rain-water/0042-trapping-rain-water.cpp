21d970e95f4ac30b0cdc26093a1c95fc32e7e342
Chintha Sri Vaishnavi <24A95A0515@aec.edu.in>
2026-06-01 20:11:40 +0530

    Time: 0 ms (100%), Space: 26.6 MB (40.35%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int trap(vector<int>& arr) {
+         int rmax=0,lmax=0;
+        int res=0;
+        vector<int> rarr(arr.size());
+        for(int i=arr.size()-1;i>=0;i--){
+            rmax=max(rmax,arr[i]);
+            rarr[i]=rmax;
+        }
+     
+        for(int i=0;i<arr.size();i++){
+            lmax=max(arr[i],lmax);
+            int water=min(lmax,rarr[i])-arr[i];
+            if(water>0)
+            res+=water;
+        }
+        return res;
+    }
+};
