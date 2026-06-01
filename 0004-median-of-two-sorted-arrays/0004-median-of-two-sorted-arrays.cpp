36d155ead6016f81a72467e194a2fb345bc646e9
Chintha Sri Vaishnavi <24A95A0515@aec.edu.in>
2026-06-01 20:55:26 +0530

    Time: 0 ms (100%), Space: 94.9 MB (94.31%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n1=nums1.size();
-        int n2=nums2.size();
-        if(n1>n2)
-        return findMedianSortedArrays(nums2,nums1);
-        int tot=(n1+n2+1)/2;
-        int low=0;
-        int high=n1;
-        int n=n1+n2;
-        while(low<=high)
-        {
-            int l1=INT_MIN;
-            int l2=INT_MIN;
-            int r1=INT_MAX;
-            int r2=INT_MAX;
-            int mid1 = low + (high - low) / 2;
-            int  mid2=tot-mid1;
-            if(mid1>0) l1=nums1[mid1-1];
-            if(mid2>0) l2=nums2[mid2-1];
-            if(mid1<n1)r1=nums1[mid1];
-            if(mid2<n2)r2=nums2[mid2];
-             if(l1<=r2 && l2<=r1 ) 
-            {
-                if(n%2 == 0) return (max(l1,l2)+min(r1,r2))/2.0;
-                else
-                return max(l1,l2);
+         int n = nums1.size();
+        int m = nums2.size();
+        int i = 0, j = 0, m1 = 0, m2 = 0;
+        for (int count = 0; count <= (n + m) / 2; count++) {
+            m2 = m1;
+            if (i != n && j != m) {
+                if (nums1[i] > nums2[j]) {
+                    m1 = nums2[j++];
+                } else {
+                    m1 = nums1[i++];
+                }
+            } else if (i < n) {
+                m1 = nums1[i++];
+            } else {
+                m1 = nums2[j++];
             }
-            else if(l1>r2) high=mid1-1;
-            else  low=mid1+1;
-
         }
-        return 0.0;
-    }     
-    
+        if ((n + m) % 2 == 1) {
+            return static_cast<double>(m1);
+        } else {
+            double ans = static_cast<double>(m1) + static_cast<double>(m2);
+            return ans / 2.0;
+        }
+    }
 };
