5f4725eb47a5ae009405e820e999f798424878fa
Chintha Sri Vaishnavi <24A95A0515@aec.edu.in>
2026-06-01 19:52:52 +0530

    Time: 0 ms (100%), Space: 14.1 MB (95.51%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
@@ -4,13 +4,10 @@ public:
        if(cost.size()<=2) return accumulate(cost.begin(),cost.end(),0);
        int  minicost=0; 
        sort(cost.begin(),cost.end());
-       int j=cost.size()-1;
-       while(j>1){
-          minicost+=(cost[j]+cost[j-1]);
-          j--,j--;
-          if(j>=0) j--;
-       }
-       for(int i=0;i<=j;i++) minicost+=cost[i];
+        for(int i=cost.size()-1; i>=0; i=i-3){
+            if(i-1>=0) minicost += cost[i] + cost[i-1];
+            else minicost += cost[i];
+        }
        return minicost;
     }
 };
