a26c295fba93b994ceaf6eb5ec1c0b66b2161320
Chintha Sri Vaishnavi <24A95A0515@aec.edu.in>
2026-06-01 21:32:45 +0530

    Time: 0 ms (100%), Space: 20 MB (80.92%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-         int maxProfit = 0;
-        for (int i = 1; i < prices.size(); i++) {
+        int profit = 0;
+        for (int i = 1; i < static_cast<int>(prices.size()); ++i) {
             if (prices[i] > prices[i - 1]) {
-                maxProfit += prices[i] - prices[i - 1];
+                profit += prices[i] - prices[i - 1];
             }
         }
-        return maxProfit;
+
+        return profit;
     }
 };
