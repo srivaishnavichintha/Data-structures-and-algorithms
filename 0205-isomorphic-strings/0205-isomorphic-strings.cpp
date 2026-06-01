572c66dc484a31df4fb3b338535be37279d55b18
Chintha Sri Vaishnavi <24A95A0515@aec.edu.in>
2026-06-01 21:50:45 +0530

    Time: 0 ms (100%), Space: 9.3 MB (81.35%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        if (s.length() != t.length()) return false;
-
-        unordered_map<char, char> mapST;
-        unordered_map<char, char> mapTS;
-
-        for (int i = 0; i < s.length(); i++) {
-            char sc = s[i], tc = t[i];
-            if (mapST.count(sc) && mapST[sc] != tc) return false;
-            if (mapTS.count(tc) && mapTS[tc] != sc) return false;
-
-            mapST[sc] = tc;
-            mapTS[tc] = sc;
+        vector<int> indexS(200, 0); 
+        vector<int> indexT(200, 0); 
+        int len = s.length(); 
+        
+        if(len != t.length()) { 
+            return false;
         }
+        
+        for(int i = 0; i < len; i++) { 
+            if(indexS[s[i]] != indexT[t[i]]) { 
+                return false; 
+            }
+            
+            indexS[s[i]] = i + 1; 
+            indexT[t[i]] = i + 1;
+        }
+        
         return true;
     }
 };
