dcfbe0da4cc543a129df3fefe880cf66960578a8
Chintha Sri Vaishnavi <24A95A0515@aec.edu.in>
2026-06-02 20:32:19 +0530

    Time: 11 ms (60.78%), Space: 14.1 MB (33.57%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
+         sort(nums.begin(), nums.end());
         int result = nums[0] + nums[1] + nums[2];
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-2;i++){
-        if(i>0 && nums[i]==nums[i-1]) continue;
-           int l=i+1;
-           int r=nums.size()-1;
-           while(l<r){
-               int sum=nums[i]+nums[l]+nums[r];
-                if(abs(sum-target)<abs(result-target)){
-                    result=sum;
-                }
-                if(target ==sum) return sum;
-                else if (sum > target)   r--;
-                else   l++;
-           }
+
+        for (int i = 0; i < nums.size() - 2; i++) {
+            int left = i + 1, right = nums.size() - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (abs(target - sum) < abs(target - result))
+                    result = sum;
+
+                if (sum == target) return target;
+                else if (sum < target) left++;
+                else right--;
+            }
         }
+
         return result;
     }
 };
