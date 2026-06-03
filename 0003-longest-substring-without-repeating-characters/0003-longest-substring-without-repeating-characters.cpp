3867c52e798a8e44dcfd0331626dbcfb0e3ab0a5
Chintha Sri Vaishnavi <24A95A0515@aec.edu.in>
2026-06-03 09:36:33 +0530

    Time: 12 ms (42.85%), Space: 11.9 MB (46.75%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -3,16 +3,14 @@ public:
     int lengthOfLongestSubstring(string s) {
       unordered_map<char,int> mp;
       if(s.size()==0) return 0;
-      int maxi=INT_MIN;
+      int maxi=0;
       int i=0;
       for(int j=0;j<s.size();j++){
-         while(mp.count(s[j])){
-             mp[s[i]]--;
-             if(mp[s[i]]==0) mp.erase(s[i]);
-             i++;
+         if(mp.count(s[j]) && mp[s[j]]>=i){
+             i=mp[s[j]]+1;
          }
-         mp[s[j]]++;
          maxi=max(maxi,j-i+1);
+         mp[s[j]]=j;
       }
       return maxi;
     }
