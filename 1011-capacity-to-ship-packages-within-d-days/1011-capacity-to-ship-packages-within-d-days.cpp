bcb7c721fcd4cb3bdb8a2a4d9c79dd392734bff2
Chintha Sri Vaishnavi <24A95A0515@aec.edu.in>
2026-06-05 09:33:59 +0530

    Time: 11 ms (67.07%), Space: 35 MB (87.45%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    long long calcd(vector<int>& weights, int cap){
+        long long ans=1;
+        long long sum=0;
+        for(int i=0;i<weights.size();i++){
+           if(sum+weights[i]>cap){
+              ans++;
+              sum=weights[i];
+           }
+           else sum+=weights[i];
+        }
+        return ans;
+    }
+    int shipWithinDays(vector<int>& weights, int days) {
+        int maxi=INT_MIN,sum=0;
+        for(int i=0;i<weights.size();i++){
+            maxi=max(weights[i],maxi);
+            sum+=weights[i];
+        }
+        int res;
+        int low=maxi,high=sum;
+        while(low<=high){
+            int mid=low+(high-low) /2;
+            long long ans=calcd(weights,mid);
+            if(ans<=days){
+                res=mid;
+                high=mid-1;
+            }
+            else low=mid+1;
+        }
+        return res;
+    }
+};
