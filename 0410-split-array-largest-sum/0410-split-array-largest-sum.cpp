203b3e4daf489c86c8aefebc01373fc58b05a3be
Chintha Sri Vaishnavi <24A95A0515@aec.edu.in>
2026-06-05 20:23:08 +0530

    Time: 0 ms (100%), Space: 10.6 MB (11.02%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    long long calc(vector<int>& nums, int lim){
+       long long k=1;
+       long long sum=0;
+       for(int i=0;i<nums.size();i++){
+          if(sum+nums[i]<=lim){
+            sum+=nums[i];
+          }
+          else{
+            k++;
+            sum=nums[i];
+          }       
+       }
+       return k;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        int ans;
+        int maxi=INT_MIN;
+        long long sum=0;
+        for(int i=0;i<nums.size();i++){
+            maxi=max(maxi,nums[i]);
+            sum+=nums[i];
+        }
+        int low=maxi;
+        long long high=sum;
+        while(low<=high){
+            long long mid= low+(high-low) /2;
+            if(calc(nums,mid)>k){
+                low=mid+1;
+            }
+            else{ 
+                ans=mid;
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+};
