0b69627fbcff19a4efab984e59f96763304d5177
Chintha Sri Vaishnavi <24A95A0515@aec.edu.in>
2026-06-08 08:05:50 +0530

    Time: 0 ms (100%), Space: 9.4 MB (16.33%) - LeetHub

--- 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.cpp
+++ 0769-max-chunks-to-make-sorted/0769-max-chunks-to-make-sorted.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
-         vector<int> prefix(arr.size());
-         int sum=0,temp=0;
-         int count=0;
-         for(int i=0;i<arr.size();i++){
-            sum += arr[i];
-            temp += i;
-            if (sum == temp) {
-                count++;
+        //  vector<int> prefix(arr.size());
+        //  int sum=0,temp=0;
+        //  int count=0;
+        //  for(int i=0;i<arr.size();i++){
+        //     sum += arr[i];
+        //     temp += i;
+        //     if (sum == temp) {
+        //         count++;
+        //     }
+        //  }
+        //  return count;
+         stack<int> st;
+        for(int i=0;i<arr.size();i++){
+            if (st.empty()||arr[i]>= st.top()) {
+                st.push(arr[i]);
             }
-         }
-         return count;
+            else {
+                int mx = st.top();
+                while (!st.empty() && st.top() > arr[i]) {
+                    st.pop();
+                }
+                st.push(mx);
+            }
+        }
+        return st.size();
     }
 };
