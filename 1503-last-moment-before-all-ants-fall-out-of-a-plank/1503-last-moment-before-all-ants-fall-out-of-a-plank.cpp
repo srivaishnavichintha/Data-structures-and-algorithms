81e569debef3aaaf810867577172cd5d993866cf
Chintha Sri Vaishnavi <24A95A0515@aec.edu.in>
2026-06-08 18:28:12 +0530

    Time: 0 ms (100%), Space: 26.9 MB (16.86%) - LeetHub

---
+++ 1503-last-moment-before-all-ants-fall-out-of-a-plank/1503-last-moment-before-all-ants-fall-out-of-a-plank.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+       int maxi=0;
+       for(int i=0;i<left.size();i++){
+        maxi=max(maxi,left[i]);
+       } 
+       for(int i=0;i<right.size();i++){
+        maxi=max(maxi,n-right[i]);
+       }
+       return maxi;
+    }
+};
