52104348e0b6933f689e4063e3be17d9f9b75534
Chintha Sri Vaishnavi <24A95A0515@aec.edu.in>
2026-06-13 14:35:56 +0530

    Time: 1 ms (37.64%), Space: 43.7 MB (24.57%) - LeetHub

---
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string str;
+        for(int i=0;i<words.size();i++){
+           string s=words[i];
+           int sum=0;
+           for(int i=0;i<s.size();i++){
+                sum+=weights[s[i] - 'a'];
+           }
+           sum%=26;
+           sum=26-sum-1;
+           str.push_back('a'+sum);
+        }
+        return str;
+    }
+};
