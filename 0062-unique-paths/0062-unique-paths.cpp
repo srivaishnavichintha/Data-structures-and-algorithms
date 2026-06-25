e8ad4cc66c81527c2aa88e32b703ec15084e1a90
Chintha Sri Vaishnavi <24A95A0515@aec.edu.in>
2026-06-25 19:57:42 +0530

    Time: 3 ms (9.57%), Space: 8.4 MB (72.09%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> aboveRow(n, 1);
+        for (int row = 1; row < m; row++) {
+            vector<int> currentRow(n, 1);
+            for (int col = 1; col < n; col++) {
+                currentRow[col] = currentRow[col - 1] + aboveRow[col];
+            }
+            aboveRow = currentRow;
+        }
+        return aboveRow[n - 1];
+    }
+};
