c5e43e5838562897f94c1be90ca9e7d5495d35ee
Chintha Sri Vaishnavi <24A95A0515@aec.edu.in>
2026-06-25 19:58:46 +0530

    Time: 1 ms (26.67%), Space: 9.5 MB (29.21%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<int> aboveRow(n, 1);
-        for (int row = 1; row < m; row++) {
-            vector<int> currentRow(n, 1);
-            for (int col = 1; col < n; col++) {
-                currentRow[col] = currentRow[col - 1] + aboveRow[col];
+        vector<vector<int>> grid(m,vector<int>(n));
+        if(m==1||n==1)
+            return 1;
+        for(int i=0;i<n;i++)
+            grid[m-1][i]=1;
+        for(int i=0;i<m;i++)
+            grid[i][n-1]=1;
+        for(int i=m-2;i>=0;i--){
+            for(int j=n-2;j>=0;j--){
+                grid[i][j]=grid[i][j+1]+grid[i+1][j];
             }
-            aboveRow = currentRow;
         }
-        return aboveRow[n - 1];
+        return grid[0][0];
     }
 };
