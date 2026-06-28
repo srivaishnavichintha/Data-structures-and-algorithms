bb32065f6be638155b6ecac4d2fd4b7e9867e2fb
Chintha Sri Vaishnavi <24A95A0515@aec.edu.in>
2026-06-28 18:34:54 +0530

    Time: 31 ms (64.05%), Space: 72.5 MB (26.61%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -4,7 +4,7 @@ public:
         priority_queue<int,vector<int>, greater<int>> pq;
         for(int i=0;i<nums.size();i++){
             pq.push(nums[i]);
-            while(pq.size()>k){
+            if(pq.size()>k){
                 pq.pop();
             }
         }
