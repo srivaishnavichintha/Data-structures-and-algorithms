3b00ab65376699c2ecf3be386e4e7cc3baaec364
Chintha Sri Vaishnavi <24A95A0515@aec.edu.in>
2026-07-05 19:49:07 +0530

    Time: 2 ms (63.59%), Space: 13.5 MB (50.14%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.cpp
+++ 0224-basic-calculator/0224-basic-calculator.cpp
@@ -1,43 +1,41 @@
 class Solution {
 public:
     int calculate(string s) {
-        long long result = 0;
-        long long num = 0;
-        int sign = 1;
-        stack<long long> st;
-
-        for (int i = 0; i < s.size(); i++) {
-            char ch = s[i];
-
-            if (isdigit(ch)) {
-                num = num * 10 + (ch - '0');
+        stack<pair<int,int>> st;
+        long long res=0,sign=1;
+        long long num=0;
+        for(char ch:s){
+            if(isdigit(ch)){
+                 num = num * 10 + (ch - '0');
             }
-            else if (ch == '+') {
-                result += sign * num;
-                num = 0;
-                sign = 1;
+            else if(ch==' ')continue;
+            else if(ch=='('){
+                st.push({res,sign});
+                res=0;sign=1;
+                num=0;
             }
-            else if (ch == '-') {
-                result += sign * num;
-                num = 0;
-                sign = -1;
+            else if(ch=='+'){
+                res+=(num*sign);
+                sign=1;
+                num=0;
             }
-            else if (ch == '(') {
-                st.push(result);
-                st.push(sign);
-                result = 0;
-                sign = 1;
+            else if(ch=='-'){
+                res+=(num*sign);
+                sign=-1;
+                num=0;
             }
-            else if (ch == ')') {
-                result += sign * num;
-                num = 0;
-
-                result *= st.top(); st.pop();
-                result += st.top(); st.pop();
+            else if(ch==')'){
+                pair<int, int> tele= st.top();
+                int r=tele.first;
+                int s=tele.second;
+                res += sign * num;
+                res  =r+ s*res;
+                st.pop();
+                num=0;
             }
         }
+        res += sign * num;
+        return res;
 
-        result += sign * num;
-        return (int)result;
     }
 };
