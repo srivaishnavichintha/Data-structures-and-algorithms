da460c095c9aedb0277a796bda7850f83c72b5ae
Chintha Sri Vaishnavi <24A95A0515@aec.edu.in>
2026-07-13 07:16:04 +0530

    Time: 25 ms (72.6%), Space: 16.1 MB (96.73%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -2,20 +2,14 @@ class Solution {
 public:
     int dr[4]={-1,1,0,0};
     int dc[4]={0,0,-1,1};
-
     void dfs(int i,int j,vector<vector<char>>& grid){
-
         int m=grid.size();
         int n=grid[0].size();
-
         if(i<0 || j<0 || i>=m || j>=n)
             return;
-
         if(grid[i][j]=='0')
             return;
-
         grid[i][j]='0';
-
         for(int k=0;k<4;k++)
             dfs(i+dr[k],j+dc[k],grid);
     }
@@ -23,21 +17,15 @@ public:
     int numIslands(vector<vector<char>>& grid) {
          int m=grid.size();
         int n=grid[0].size();
-
         int ans=0;
-
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-
                 if(grid[i][j]=='1'){
                     ans++;
                     dfs(i,j,grid);
                 }
-
             }
         }
-
         return ans;
-
     }
 };
