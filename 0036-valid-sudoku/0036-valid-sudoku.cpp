a4ab04b3fc469f916b9b7a0cba7a1233773f2494
Chintha Sri Vaishnavi <24A95A0515@aec.edu.in>
2026-07-22 13:12:04 +0530

    Time: 0 ms (100%), Space: 21.9 MB (75.14%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -4,7 +4,6 @@ public:
         bool rows[9][9] = {false};
         bool cols[9][9] = {false};
         bool boxes[9][9] = {false};
-
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 if (board[i][j] != '.') {
