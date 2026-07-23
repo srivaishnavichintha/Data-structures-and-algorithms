297c2b34c600a4ff6d990e1dbd0e6449cb941281
Chintha Sri Vaishnavi <24A95A0515@aec.edu.in>
2026-07-23 21:05:45 +0530

    Time: 0 ms (100%), Space: 19.6 MB (34.15%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-         int start = 0;
+        int start = 0;
         int end = numbers.size() - 1;
-
         while(start < end){
-
             int sum = numbers[start] + numbers[end];
-
             if(sum == target){
                 return {start + 1, end + 1};
             }
@@ -18,7 +15,6 @@ public:
                 end--;
             }
         }
-
         return {-1, -1};
     }
 };
