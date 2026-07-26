a3cc76d3099dcb0ea0493159489cb13f75c49943
Chintha Sri Vaishnavi <24A95A0515@aec.edu.in>
2026-07-26 18:51:06 +0530

    Time: 30 ms (5.37%), Space: 31.5 MB (76.45%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
          sort(nums.begin(),nums.end());
-        return max(
-            nums.back()*nums[nums.size() - 2] *nums[nums.size() - 3],
-            nums.back() *nums.front() *nums[1]
-        );
+        return max( nums.back()*nums[nums.size() - 2] *nums[nums.size() - 3],
+            nums.back() *nums.front() *nums[1]);
     }
 };
