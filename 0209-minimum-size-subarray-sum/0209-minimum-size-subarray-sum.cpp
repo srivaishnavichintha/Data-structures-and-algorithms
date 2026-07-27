8e89581da2852b0e56582a1e392a56db48d2a9ca
Chintha Sri Vaishnavi <24A95A0515@aec.edu.in>
2026-07-27 20:32:27 +0530

    Time: 0 ms (100%), Space: 42 MB (28.62%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        int sum=0,minlen=INT_MAX;
-        // unordered_map<int,int> mp;
-        int l=0;
-        for(int i=0;i<nums.size();i++){
-            sum+=nums[i];
-            while(sum>=target){
-                minlen=min(minlen,i-l+1);
-                sum-=nums[l];
-                l++;
-            }
-        }
-        return minlen==INT_MAX ? 0 : minlen;
+        int mini = INT_MAX;
+        int i=0,sum=0;
+        for(int j=0;j<nums.size();j++){
+             sum+=nums[j];
+             while(sum>=target){
+                 mini=min(mini,j-i+1);
+                 sum-=nums[i];
+                 i++;
+             }
+        } 
+        return mini == INT_MAX ? 0 : mini;
     }
 };
