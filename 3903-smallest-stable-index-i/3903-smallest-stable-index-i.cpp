e030124dae86d5772f662b1fe4f31ad150a0e345
Chintha Sri Vaishnavi <24A95A0515@aec.edu.in>
2026-09-04 10:55:14 +0530

    Time: 0 ms (100%), Space: 31.2 MB (46.94%) - LeetHub

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        vector<int> pre(nums.size(),0);
+        int mini=INT_MAX;
+        for(int i=nums.size()-1;i>=0;i--){
+             mini=min(mini,nums[i]);
+             pre[i]=mini;
+        }
+        mini=INT_MIN;
+        for(int i=0;i<nums.size();i++){
+            mini=max(mini,nums[i]);
+            int temp=abs(mini-pre[i]);
+            if(temp<=k)return i;
+        }
+        return -1;
+    }
+};
