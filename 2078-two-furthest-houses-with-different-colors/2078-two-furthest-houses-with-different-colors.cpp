a7008cda84d8b563c131799c8e16b5aef24b50f3
Chintha Sri Vaishnavi <24A95A0515@aec.edu.in>
2026-09-17 21:59:26 +0530

    Time: 1 ms (5.44%), Space: 12 MB (71.71%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
@@ -1,17 +1,7 @@
 class Solution {
 public:
     int maxDistance(vector<int>& colors) {
-        // int maxdis=0;
-        // for(int i=0;i<colors.size();i++){
-        //     for(int j=colors.size()-1;j>i;j--){
-        //         if(colors[j]!=colors[i]){
-        //             maxdis=max(abs(j-i),maxdis);
-        //             break;
-        //         } 
-        //     }
-        // }
-        // return maxdis;
-        int n = colors.size();
+         int n = colors.size();
         int maxdis = 0;
 
         for(int i = 0; i < n; i++){
