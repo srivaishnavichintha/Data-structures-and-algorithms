eae805f3c20f99a102717fbbed708309e52036eb
Chintha Sri Vaishnavi <24A95A0515@aec.edu.in>
2026-09-22 11:08:16 +0530

    Time: 168 ms (38.39%), Space: 173.1 MB (28.57%) - LeetHub

--- 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.cpp
+++ 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.cpp
@@ -3,7 +3,6 @@ public:
     vector<long long> resultArray(vector<int>& nums, int k) {
         vector<long long> res(k,0);
         vector<vector<long long>> dp(nums.size(),vector<long long>(k,0));
-        vector<long long> prefix(nums.size()+1,1),suffix(nums.size()+1,1);
         dp[0][nums[0] % k] = 1;
         res[nums[0] % k]++;
         for(int i = 1; i < nums.size(); i++) {
